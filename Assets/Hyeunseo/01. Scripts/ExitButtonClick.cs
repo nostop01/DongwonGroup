6e843e2f403c924fd733a774801536825304c24d
stemcill <stemcill0812@gmail.com>
2023-11-03 00:44:54 +0900

    TitleScene+TunaAsset

---
+++ Assets/Hyeunseo/01. Scripts/ExitButtonClick.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitButtonClick : MonoBehaviour
+{
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
