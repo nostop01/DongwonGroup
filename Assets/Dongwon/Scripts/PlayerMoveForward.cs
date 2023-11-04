5acc92521b1c735112567453776032f41d1ed2b7
nostop01 <98877352+nostop01@users.noreply.github.com>
2023-11-04 17:40:25 +0900

    23.11.04
    변경점 디코에 있음

--- Assets/Dongwon/Scripts/PlayerMoveForward.cs
+++ Assets/Dongwon/Scripts/PlayerMoveForward.cs
@@ -7,10 +7,13 @@ public class PlayerMoveForward : MonoBehaviour
     public float MoveSpeed = 5f;
     public bool CanMove = true; //게임오버가 아닐 때, 게임이 끝나지 않았을 때 움직이도록 하는 변수
 
+    Rigidbody _rigid;
+    Vector3 getVel;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigid = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -18,7 +21,9 @@ public class PlayerMoveForward : MonoBehaviour
     {
         if(CanMove) //움직일 수 있는 상태일때
         {
-            transform.Translate(new Vector3(0.0f, MoveSpeed * Time.deltaTime, 0.0f)); //무조건 앞으로 나아감
+            getVel = new Vector3(MoveSpeed, 0.0f, 0.0f);
+
+            _rigid.velocity = getVel;  //무조건 앞으로 나아감
         }
     }
 }
