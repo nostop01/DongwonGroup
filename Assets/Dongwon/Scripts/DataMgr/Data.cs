d879d2cc8aa6cfcbffb1ee9d1c2dad85ec2b11df
nostop01 <98877352+nostop01@users.noreply.github.com>
2023-11-11 17:50:14 +0900

    23.11.11
    DataManager 끝냄

---
+++ Assets/Dongwon/Scripts/DataMgr/Data.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+
+public class Data //원하는 데이터/데이터 형식/갯수 등으로 저장.
+{
+    public bool[] isUnlock = new bool[8]; //스테이지 클리어 상태
+
+    public float[] SettingState = new float[3]; //설정값 저장
+}
