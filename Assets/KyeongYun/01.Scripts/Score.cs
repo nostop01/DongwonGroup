50f84c60d62cfa699878d2e27af2984169cf37cc
kylee37 <kylee2998@gmail.com>
2023-11-15 09:19:14 +0900

    Coin
    코인 추가 및 점수

---
+++ Assets/KyeongYun/01.Scripts/Score.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    Text text;
+    public static int coinAmount;
+    void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        text.text = coinAmount.ToString(); ;
+    }
+}
