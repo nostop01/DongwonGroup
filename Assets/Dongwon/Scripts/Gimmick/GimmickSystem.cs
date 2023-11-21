35e6f058f4641dd4dc3fbef0341609fbbf688d99
nostop01 <nstop010@daum.net>
2023-11-21 22:51:08 +0900

    23.11.21

--- Assets/Dongwon/Scripts/Gimmick/GimmickSystem.cs
+++ Assets/Dongwon/Scripts/Gimmick/GimmickSystem.cs
@@ -40,7 +40,7 @@ public class GimmickSystem : MonoBehaviour
 
         if(!PlayerStatus.Instance.Death) //플레이어가 Death 상태가 아닐 경우
         {
-            if(!PlayerStatus.Instance.Pause) //게임이 퍼즈가 아닐 경우
+            if(!PlayerStatus.Instance.Pause && !PlayerStatus.Instance.Clear) //게임이 퍼즈가 아닐 경우
             {
                 PlayerTrmCheck(); //플레이어가 제자리에서 5초동안 움직이지 않을경우 동작하는 함수
                 TenSecSpawnSpear(); //10초마다 동작하는 기믹 시스템 함수
--- Assets/Dongwon/Scripts/UI/BackToLobby.cs
+++ Assets/Dongwon/Scripts/UI/BackToLobby.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class BackToLobby : MonoBehaviour
 {
-    
+    public string sceneName;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
 }
